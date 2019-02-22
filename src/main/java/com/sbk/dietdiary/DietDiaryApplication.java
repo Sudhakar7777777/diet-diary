abffc49590f0eaa76e83537d939cf7ef95355d11
Sudhakar Balakrishnan <>
2019-02-21 21:27:35 -0800

    Create base template

---
+++ src/main/java/com/sbk/dietdiary/DietDiaryApplication.java
@@ -0,0 +1,15 @@
+package com.sbk.dietdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietDiaryApplication.class, args);
+	}
+}
+
+
+
