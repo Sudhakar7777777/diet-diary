57dd9e3e441793fa1a59dea9c3982689a61d7d3e
Sudhakar Balakrishnan <>
2019-03-23 20:55:50 -0700

    commit

--- src/main/java/com/sbk/dietdiary/GreetingController.java
+++ src/main/java/com/sbk/dietdiary/GreetingController.java
@@ -1,5 +1,7 @@
 package com.sbk.dietdiary;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +11,26 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class GreetingController
 {
+	private static Logger logger = LoggerFactory.getLogger(GreetingController.class);
+
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
 	@RequestMapping("/greeting")
 	public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+		logger.info("In method greeting...");
 		return new Greeting(counter.incrementAndGet(),
 				String.format(template, name));
+	}
+
+	@RequestMapping("/")
+	public String index() {
+		logger.trace("A TRACE Message");
+		logger.debug("A DEBUG Message");
+		logger.info("An INFO Message");
+		logger.warn("A WARN Message");
+		logger.error("An ERROR Message");
+
+		return "Howdy! Check out the Logs to see the output...";
 	}
 }
